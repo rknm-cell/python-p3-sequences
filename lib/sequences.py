835b12c28a6ac0c3c00bd73986a0788f7f2fcb35
rknm-cell <racknahm@gmail.com>
2023-05-01 16:22:05 -0400

    completed tasks

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci_list = []
+    if length > 0:
+        fibonacci_list.append(0)
+        if length > 1:
+            fibonacci_list.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_list.append(
+                        fibonacci_list[i - 1] + fibonacci_list[i - 2])
+
+    print(fibonacci_list)
